f1cbee67297ee2fb9d35bd091e145f26d34a1730
JakeS <jakesnyder314@protonmail.com>
2023-09-06 11:51:38 -0500

    Imported random lib, created a list featuring 52 playable 'cards', created class Deck.

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,1 +1,22 @@
+import random
 
+#Here is where I create a deck of cards using 3 lists
+number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"]
+suits = [u"\u2663", u"\u2660", u"\u2666", u"\u2665"]
+#using list comprehension to create the 52 playable cards
+deck = [[x, y] for x in number for y in suits]
+
+#The Deck class will feature actions that a deck of cards is capable of via a "Dealer" class
+class Deck:
+    def __init__(self, cards):
+        self.cards = cards
+        
+    def __repr__(self):
+        return str(self.cards)
+    
+    def deal_card(self):
+        pick = self.cards.pop()
+        return pick
+    
+    def shufffle_deck(self):
+        random.shuffle(self.cards)    
